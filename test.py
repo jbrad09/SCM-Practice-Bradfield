e54bf3eee9efdc7db0788af952fcec3de88b2727
jbrad09 <jbrad09@jaguar.tamu.edu>
2025-03-27 20:16:05 -0500

    Initial Commit

---
+++ test.py
@@ -0,0 +1,15 @@
+def main():
+    print("Hello, World!")
+
+def code():
+    print("I like coding!")
+
+    for _ in range(8):
+        print("code")
+
+
+if __name__ == "__main__":
+    main()
+
+if __name__ == "__main__":
+    code()
